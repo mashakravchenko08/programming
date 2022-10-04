74c2b3da72723cfd32f4878fd6ddf7a46c0cd5c2
mashakravchenko08 <113291905+mashakravchenko08@users.noreply.github.com>
2022-10-05 01:51:32 +0300

    Add files via upload

---
+++ sem 4/s4_z11.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void mult2_array(int* p, size_t n)
+{
+    for (size_t j=0; j<n; ++j)
+    {   
+        int* u = p +j;
+        *u *=2;
+    }
+
+}
+int main()
+{
+    int numbers[6] = {4, 8, 15, 16, 23, 42};
+    int* p = &numbers[0];
+    size_t n= 6;
+    mult2_array(p, n);
+    for (size_t i = 0; i < n; ++i)
+        printf("%i ", numbers[i]);
+}
