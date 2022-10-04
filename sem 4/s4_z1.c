bed035a9031f9a9042277319a225e47bc89f0afb
mashakravchenko08 <113291905+mashakravchenko08@users.noreply.github.com>
2022-10-05 01:45:26 +0300

    Add files via upload

---
+++ sem 4/s4_z1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+unsigned long long fact(int n)
+{
+    unsigned long long result = 1;
+    for (unsigned long long i = 1; i <= n; ++i)
+        result *= i;
+    return result;
+}
+int main()
+{
+    int k;
+    scanf("%i", &k);
+    printf("%llu\n", fact(k));
+}
