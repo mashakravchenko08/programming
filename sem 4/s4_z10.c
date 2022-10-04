bd987b2f0c2250f842234a017b48f704f83a69e0
mashakravchenko08 <113291905+mashakravchenko08@users.noreply.github.com>
2022-10-05 01:50:05 +0300

    Add files via upload

---
+++ sem 4/s4_z10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+float cube(float* p)
+{
+    float c = *p * *p * *p;
+    return c;
+}
+int main()
+{
+    float numbers[6] = {4.01, 8.02, 15.03, 16.04, 23.05, 42.06};
+    float* p = &numbers[3];
+    printf("%f", cube(p));
+}
